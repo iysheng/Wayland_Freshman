9bb303d8d7fb242cf7d29d2e5b3016add51ca71e
iysheng <iysheng@163.com>
2022-07-13 14:58:49 +0800

    [CHG][NT] 客户端无法显示内容

--- 19.learn_wayland/main.c
+++ 19.learn_wayland/main.c
@@ -1,13 +1,49 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <wayland-client.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/mman.h>
 
-static void
-registry_handle_global(void *data, struct wl_registry *registry,
+struct my_output {
+    struct wl_compositor * compositor;
+    struct wl_shm * shm;
+};
+
+
+void test_format(void *data,
+	       struct wl_shm *wl_shm,
+	       uint32_t format)
+{
+	static int i = 0;
+	printf("format[%d]=%x\n", i++, format);
+}
+
+
+struct wl_shm_listener shm_listener = {
+    .format = test_format,
+};
+
+static void registry_handle_global(void *data, struct wl_registry *registry,
 		uint32_t name, const char *interface, uint32_t version)
 {
+	struct my_output *state = (struct my_output *)data;
 	printf("interface: '%s', version: %d, name: %d\n",
 			interface, version, name);
+	if (!strcmp(interface, wl_compositor_interface.name))
+	{
+		/* 绑定混合器 */
+		state->compositor = wl_registry_bind(registry, name, &wl_compositor_interface, 4);
+		printf("绑定混合器\n");
+	} else if (strcmp(interface, wl_shm_interface.name) == 0) {
+        state->shm = wl_registry_bind(
+            registry, name, &wl_shm_interface, 1);
+		wl_shm_add_listener(state->shm, &shm_listener, NULL);
+		printf("绑定内存管理器\n");
+    }
 }
 
 static void
@@ -23,10 +59,80 @@ registry_listener = {
 	.global_remove = registry_handle_global_remove,
 };
 
+static int
+set_cloexec_or_close(int fd)
+{
+        long flags;
+
+        if (fd == -1)
+                return -1;
+
+        flags = fcntl(fd, F_GETFD);
+        if (flags == -1)
+                goto err;
+
+		/* 设置自动关闭这个 fd 的 flags */
+        if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
+                goto err;
+
+        return fd;
+
+err:
+        close(fd);
+        return -1;
+}
+
+static int
+create_shm_file(void)
+{
+#define NAME_TEMPLATE    "/wl_shm-XXXXXX"
+	char name[64] = {0};
+	const char *path;
+	int fd;
+
+	path = getenv("XDG_RUNTIME_DIR");
+	if (path)
+	{
+		strcpy(name, path);
+	}
+	strcat(name, NAME_TEMPLATE);
+	printf("name=%s\n", name);
+
+	/* 根据模板创建临时文件句柄 */
+	fd = mkstemp(name);
+	//fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
+    if (fd >= 0) {
+        fd = set_cloexec_or_close(fd);
+        unlink(name);
+		return fd;
+    }
+
+	return -1;
+}
+
+int
+allocate_shm_file(size_t size)
+{
+	int fd = create_shm_file();
+	int ret;
+	if (fd < 0)
+		return -1;
+	do {
+		ret = ftruncate(fd, size);
+	} while (ret < 0 && errno == EINTR);
+	if (ret < 0) {
+		close(fd);
+		return -1;
+	}
+	return fd;
+}
+
 int
 main(int argc, char *argv[])
 {
 	struct wl_display *display = wl_display_connect(NULL);
+    struct my_output state = {0};
+	struct wl_surface *surface = NULL;
 
 	if (display)
 	{
@@ -39,9 +145,60 @@ main(int argc, char *argv[])
 	}
 
 	struct wl_registry *registry = wl_display_get_registry(display);
+    const int width = 100, height = 100;
+    const int stride = width * 4;
+    const int shm_pool_size = height * stride * 2;
+    
+    struct wl_shm_pool *pool;
+    int fd = allocate_shm_file(shm_pool_size);
+    uint8_t *pool_data = mmap(NULL, shm_pool_size,
+        PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	int index = 0;
+    int offset = height * stride * index;
+    uint32_t *pixels = (uint32_t *)&pool_data[offset];
+    struct wl_buffer *buffer;
 
-	wl_registry_add_listener(registry, &registry_listener, NULL);
+	wl_registry_add_listener(registry, &registry_listener, &state);
+
 	wl_display_roundtrip(display);
+
+	if (state.compositor)
+	{
+		surface = wl_compositor_create_surface(state.compositor);
+	}
+
+	if (state.shm)
+	{
+        pool = wl_shm_create_pool(state.shm, fd, shm_pool_size);
+        buffer = wl_shm_pool_create_buffer(pool, offset,
+            width, height, stride, WL_SHM_FORMAT_XRGB8888);
+		for (int y = 0; y < height; ++y) {
+          for (int x = 0; x < width; ++x) {
+            if ((x + y / 8 * 8) % 16 < 8) {
+              pixels[y * width + x] = 0xFF666666;
+            } else {
+              pixels[y * width + x] = 0xFFEEEEEE;
+            }
+          }
+        }
+    }
+	if (!surface)
+	{
+		printf("invalid surface\n");
+		return -2;
+	}
+
+	wl_surface_attach(surface, buffer, 0, 0);
+    wl_surface_damage(surface, 0, 0, UINT32_MAX, UINT32_MAX);
+    wl_surface_commit(surface);
+	printf("buffer commit is done\n");
+
+	/* 处理接收到的 events */
+	while (-1 != wl_display_dispatch(display))
+	{
+
+	}
+
 	return 0;
 }
 
--- 20.learn_wayland_server/main.c
+++ 20.learn_wayland_server/main.c
@@ -3,12 +3,81 @@
 #include <wayland-client.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <wayland-server.h>
+
+struct my_output;
+struct my_state {
+    int a;
+    int b;
+    struct my_output *client_outputs;
+};
+
+#if 1
+struct my_output {
+    struct my_state * state;
+    struct wl_resource *resource;
+};
+
+static void
+wl_output_handle_resource_destroy(struct wl_resource *resource)
+{
+    struct my_output *client_output = wl_resource_get_user_data(resource);
+
+    // TODO: Clean up resource
+
+    //remove_to_list(client_output->state->client_outputs, client_output);
+}
+
+static void
+wl_output_handle_release(struct wl_client *client, struct wl_resource *resource)
+{
+    printf("client will done\n");
+    wl_resource_destroy(resource);
+}
+
+static const struct wl_output_interface
+wl_output_implementation = {
+    /* 这个接口所有的 request 都会执行这个函数
+     * client 发送 release request 的时候会执行这个函数
+     * */
+    .release = wl_output_handle_release,
+};
+
+static void
+wl_output_handle_bind(struct wl_client *client, void *data,
+    uint32_t version, uint32_t id)
+{
+    struct my_state *state = data;
+
+    struct my_output *client_output = calloc(1, sizeof(struct my_output));
+
+    struct wl_resource *resource = wl_resource_create(
+        client, &wl_output_implementation, wl_output_interface.version, id);
+
+    wl_resource_set_implementation(resource, &wl_output_implementation,
+        client_output, wl_output_handle_resource_destroy);
+
+    client_output->resource = resource;
+    client_output->state = state;
+
+    // TODO: Send geometry event, et al
+    wl_output_send_geometry(resource, 0, 0, 1920, 1080,
+        WL_OUTPUT_SUBPIXEL_UNKNOWN, "Foobar, Inc",
+        "Fancy Monitor 9001 4K HD 120 FPS Noscope",
+        WL_OUTPUT_TRANSFORM_NORMAL);
+    printf("Send some events\n");
+
+
+    //add_to_list(state->client_outputs, client_output);
+}
+#endif
 
 int
 main(int argc, char *argv[])
 {
     struct wl_display *display = wl_display_create();
+    struct my_state state = {1, 2};
     if (!display) {
         fprintf(stderr, "Unable to create Wayland display.\n");
         return 1;
@@ -21,10 +90,15 @@ main(int argc, char *argv[])
     }
 
     fprintf(stderr, "Running Wayland display on %s\n", socket);
+
+    wl_global_create(display, &wl_output_interface,
+        1, &state, wl_output_handle_bind);
+
     wl_display_run(display);
 
     wl_display_destroy(display);
     printf("Destory wayland display server:%s\n", socket);
+
     return 0;
 }
 
